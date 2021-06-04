5db1c0a00449bd244bbdac21fba7dd415dc91f83
brookstaylor <brooks.o.taylor@vanderbilt.edu>
2021-06-03 22:19:08 -0400

    update scripts to source from right dir

--- scripts/run_badger_bot.py
+++ scripts/run_badger_bot.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../src"))
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src"))
 )
 
 from price_bot import PriceBot
