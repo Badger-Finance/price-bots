f00f172703d3659552803384703017e9729b92bb
btcookies <31353111+btcookies@users.noreply.github.com>
2022-03-15 12:12:42 -0400

    Quick fix (#4)
    * pull monitor hook from aws, use close() instead of logout())
    
    * close not logout
    
    * try just passing out of loop on fail instead of trying to restart bot
    
    * update digg bot handling
    
    * handle webhook error sett bot

--- src/sett_bot.py
+++ src/sett_bot.py
@@ -60,18 +60,18 @@ class SettBot(PriceBot):
                             self.logger.error("Error updated nickname")
                             self.logger.error(e)
                             webhook = discord.Webhook.from_url(
-                                os.getenv("DISCORD_MONITORING_WEBHOOK_URL"),
+                                self.monitoring_webhook_url,
                                 adapter=discord.RequestsWebhookAdapter(),
                             )
                             embed = discord.Embed(
                                 title=f"**{self.token_display} Price Bot Error**",
                                 description=f"Error message: {e}",
                             )
-                            webhook.send(embed=embed, username="Price Bot Monitoring")
-                            # sleep and restart bot if breaks
-                            sleep(10)
-                            await self.logout()
-                            await self.start(self.bot_token)
+                            try:
+                                webhook.send(embed=embed, username="Price Bot Monitoring")
+                            except Exception as e:
+                                self.logger.error(f"Error sending webhook: {e}")
+                                self.logger.error("passing out of loop, hopefully restart")
 
     @update_price.before_loop
     async def before_update_price(self):
